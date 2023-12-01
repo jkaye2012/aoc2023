61c7a908ad51c8860f2daac3871edaddb1803df3
Jordan Kaye <jordan.kaye2@gmail.com>
2023-12-01 12:22:50 -0700

    Hermetic nix-shell

--- shell.nix
+++ shell.nix
@@ -1,12 +1,28 @@
-
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz") {}}:
 
 let
   fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") {};
+  aoc = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "cargo-aoc";
+    version = "0.3.5";
+
+    buildInputs = with pkgs; [ openssl ];
+
+    nativeBuildInputs = with pkgs; [ pkg-config ];
+
+    src = pkgs.fetchFromGitHub {
+      owner = "gobanos";
+      repo = pname;
+      rev = version;
+      hash = "sha256-tHuT/dsiyliXdl34bFraYp3T3FUgxFnhEUQfc8O197I=";
+    };
+
+    cargoHash = "sha256-lUQwwGJLHLI9bfJiLUUE8j1svBAgbvr+8hKB/bRzwNA=";
+  };
 in
 pkgs.mkShell {
   packages = [
     fenix.stable.toolchain
-    pkgs.openssl
+    aoc
   ];
 }
