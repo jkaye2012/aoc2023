a67a07175dbcb9cac69532a71a01a985d6986768
Jordan Kaye <jordan.kaye2@gmail.com>
2023-12-09 08:35:14 -0700

    Day 9 solutions

---
+++ src/day9.rs
@@ -0,0 +1,52 @@
+const ARITY: usize = 21;
+type SensorValue = [i64; ARITY];
+
+#[aoc_generator(day9)]
+fn generate(input: &str) -> Vec<SensorValue> {
+    input
+        .lines()
+        .map(|l| {
+            let mut value = [0i64; ARITY];
+            l.split(' ')
+                .map(|i| i.parse::<i64>().unwrap())
+                .enumerate()
+                .for_each(|(idx, val)| value[idx] = val);
+            value
+        })
+        .collect()
+}
+
+fn trends(readings: &SensorValue) -> (i64, i64) {
+    let mut last = [0i64; ARITY];
+    let mut curr = [0i64; ARITY];
+    last[0] = readings[ARITY - 1];
+    curr[0] = readings[ARITY - 1];
+    let mut idx = 0;
+    while idx != ARITY - 1 {
+        idx += 1;
+        let mut prev_diff = curr[0] - readings[ARITY - (1 + idx)];
+        curr[0] = readings[ARITY - (1 + idx)];
+        for update_idx in 1..idx {
+            let prev = prev_diff;
+            prev_diff = curr[update_idx] - prev_diff;
+            curr[update_idx] = prev;
+        }
+        last[idx] = prev_diff;
+        curr[idx] = prev_diff;
+    }
+    let mut prev = 0;
+    for i in (1..ARITY - 1).rev() {
+        prev = curr[i - 1] - prev;
+    }
+    (prev, last.iter().sum())
+}
+
+#[aoc(day9, part1)]
+fn sensor_next(input: &[SensorValue]) -> i64 {
+    input.iter().map(|sv| trends(sv).1).sum()
+}
+
+#[aoc(day9, part2)]
+fn sensor_prev(input: &[SensorValue]) -> i64 {
+    input.iter().map(|sv| trends(sv).0).sum()
+}
--- src/util.rs
+++ src/util.rs
@@ -10,7 +10,7 @@ pub fn lcm(a: usize, b: usize) -> usize {
     (a * b) / gcd(a, b)
 }
 
-pub fn lcm_multiple(numbers: &[usize]) -> usize {
+pub fn _lcm_multiple(numbers: &[usize]) -> usize {
     let first = numbers[0];
     numbers.iter().skip(1).fold(first, |l, &x| lcm(x, l))
 }
