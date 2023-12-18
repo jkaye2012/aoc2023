aaf89195fa11f202155bf8d960d1537de04a5c23
Jordan Kaye <jordan.kaye2@gmail.com>
2023-12-18 12:01:01 -0700

    Day 18 tweak

--- src/day18.rs
+++ src/day18.rs
@@ -1,38 +1,13 @@
-#[aoc_generator(day18, part1)]
-pub fn generate1(input: &str) -> Vec<(isize, isize, isize)> {
+fn generate<F>(input: &str, gen: F) -> Vec<(isize, isize, isize)>
+where
+    F: Fn(&str) -> (isize, isize),
+{
     let mut prev = (0isize, 0isize);
     input
         .lines()
         .rev()
         .map(|line| {
-            let mut it = line.split(' ');
-            let dir = it.next().unwrap();
-            let mag = it.next().unwrap().parse::<isize>().unwrap();
-            let (y, x) = match dir {
-                "R" => (prev.0, prev.1 + mag),
-                "D" => (prev.0 + mag, prev.1),
-                "L" => (prev.0, prev.1 - mag),
-                "U" => (prev.0 - mag, prev.1),
-                _ => panic!("Invalid direction"),
-            };
-
-            prev = (y, x);
-            (y, x, mag)
-        })
-        .collect()
-}
-
-#[aoc_generator(day18, part2)]
-pub fn generate2(input: &str) -> Vec<(isize, isize, isize)> {
-    let mut prev = (0isize, 0isize);
-    input
-        .lines()
-        .rev()
-        .map(|line| {
-            let it = line.split(' ');
-            let hex = u32::from_str_radix(&it.skip(2).next().unwrap()[2..8], 16).unwrap();
-            let dir = hex & 0xF;
-            let mag = (hex >> 4) as isize;
+            let (dir, mag) = gen(line);
             let (y, x) = match dir {
                 0 => (prev.0, prev.1 + mag),
                 1 => (prev.0 + mag, prev.1),
@@ -47,6 +22,37 @@ pub fn generate2(input: &str) -> Vec<(isize, isize, isize)> {
         .collect()
 }
 
+#[aoc_generator(day18, part1)]
+pub fn generate1(input: &str) -> Vec<(isize, isize, isize)> {
+    let gen = |line: &str| {
+        let mut it = line.split(' ');
+        let dir = it.next().unwrap();
+        let mag = it.next().unwrap().parse::<isize>().unwrap();
+        (
+            match dir {
+                "R" => 0,
+                "D" => 1,
+                "L" => 2,
+                "U" => 3,
+                _ => panic!("Invalid direction"),
+            },
+            mag,
+        )
+    };
+    generate(input, gen)
+}
+
+#[aoc_generator(day18, part2)]
+pub fn generate2(input: &str) -> Vec<(isize, isize, isize)> {
+    let gen = |line: &str| {
+        let it = line.split(' ');
+        let hex = u32::from_str_radix(&it.skip(2).next().unwrap()[2..8], 16).unwrap();
+        let dir = hex & 0xF;
+        let mag = hex >> 4;
+        (dir as isize, mag as isize)
+    };
+    generate(input, gen)
+}
 #[aoc(day18, part1)]
 #[aoc(day18, part2)]
 pub fn lava_trench_area(input: &[(isize, isize, isize)]) -> isize {
